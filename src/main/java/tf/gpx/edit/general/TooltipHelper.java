8cbb618c8cc4c1a871578cb6b2ca47d0795ade4b
Thomas Feuster <shopping@feuster.com>
2018-11-12 20:25:07 +0100

    1st working version
    - Various updates to build.gradle
    - Added AboutMenu
    - Removed pre Java9 code from TooltipHelper
    - Extracted private classes in Tests

--- src/main/java/tf/gpx/edit/general/TooltipHelper.java
+++ src/main/java/tf/gpx/edit/general/TooltipHelper.java
@@ -25,9 +25,6 @@
  */
 package tf.gpx.edit.general;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import javafx.scene.control.Tooltip;
 import javafx.util.Duration;
 
@@ -66,34 +63,34 @@ public class TooltipHelper {
             final boolean hideOnExit) {
         // TFE, 20181005: with java 9 its no longer necessary to use reflection
         // since the required methods of behaviour are now available
-//        tooltip.setShowDelay(Duration.millis(openDelay));
-//        tooltip.setHideDelay(Duration.millis(closeDelay));
-//        tooltip.setShowDuration(Duration.millis(visibleDuration));
-//        tooltip.setAutoHide(hideOnExit);
-//        tooltip.setHideOnEscape(hideOnExit);
-//        tooltip.setAutoFix(true);
-        try {
-            // Get the non public field "BEHAVIOR"
-            Field fieldBehavior = tooltip.getClass().getDeclaredField("BEHAVIOR");
-            // Make the field accessible to be able to get and set its value
-            fieldBehavior.setAccessible(true);
-            // Get the value of the static field
-            Object objBehavior = fieldBehavior.get(null);
-            // Get the constructor of the private static inner class TooltipBehavior
-            Constructor<?> constructor = objBehavior.getClass().getDeclaredConstructor(
-                Duration.class, Duration.class, Duration.class, boolean.class
-            );
-            // Make the constructor accessible to be able to invoke it
-            constructor.setAccessible(true);
-            // Create a new instance of the private static inner class TooltipBehavior
-            Object tooltipBehavior = constructor.newInstance(
-                new Duration(openDelay), new Duration(visibleDuration),
-                new Duration(closeDelay), hideOnExit
-            );
-            // Set the new instance of TooltipBehavior
-            fieldBehavior.set(null, tooltipBehavior);
-        } catch (IllegalAccessException | IllegalArgumentException | InstantiationException | NoSuchFieldException | NoSuchMethodException | SecurityException | InvocationTargetException e) {
-            throw new IllegalStateException(e);
-        }
+        tooltip.setShowDelay(Duration.millis(openDelay));
+        tooltip.setHideDelay(Duration.millis(closeDelay));
+        tooltip.setShowDuration(Duration.millis(visibleDuration));
+        tooltip.setAutoHide(hideOnExit);
+        tooltip.setHideOnEscape(hideOnExit);
+        tooltip.setAutoFix(true);
+//        try {
+//            // Get the non public field "BEHAVIOR"
+//            Field fieldBehavior = tooltip.getClass().getDeclaredField("BEHAVIOR");
+//            // Make the field accessible to be able to get and set its value
+//            fieldBehavior.setAccessible(true);
+//            // Get the value of the static field
+//            Object objBehavior = fieldBehavior.get(null);
+//            // Get the constructor of the private static inner class TooltipBehavior
+//            Constructor<?> constructor = objBehavior.getClass().getDeclaredConstructor(
+//                Duration.class, Duration.class, Duration.class, boolean.class
+//            );
+//            // Make the constructor accessible to be able to invoke it
+//            constructor.setAccessible(true);
+//            // Create a new instance of the private static inner class TooltipBehavior
+//            Object tooltipBehavior = constructor.newInstance(
+//                new Duration(openDelay), new Duration(visibleDuration),
+//                new Duration(closeDelay), hideOnExit
+//            );
+//            // Set the new instance of TooltipBehavior
+//            fieldBehavior.set(null, tooltipBehavior);
+//        } catch (IllegalAccessException | IllegalArgumentException | InstantiationException | NoSuchFieldException | NoSuchMethodException | SecurityException | InvocationTargetException e) {
+//            throw new IllegalStateException(e);
+//        }
     }
 }
